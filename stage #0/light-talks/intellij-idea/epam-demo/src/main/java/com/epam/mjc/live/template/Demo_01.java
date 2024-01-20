c2746f12c3e10bb0298ca736c201bf3a3566d606
Arsen Asatryan <arsen_asatryan@epam.com>
2024-01-21 00:03:50 +0400

    first commit

---
+++ stage #0/light-talks/intellij-idea/epam-demo/src/main/java/com/epam/mjc/live/template/Demo_01.java
@@ -0,0 +1,13 @@
+package com.epam.mjc.live.template;
+
+
+/**
+ * Show live templates usage
+ * {@code psvm} - main method
+ * {@code fori} - cycle
+ * {@code sout} - System.out.println
+ */
+public class Demo_01 {
+
+    // Task: print "Hello World" 10 times
+}
